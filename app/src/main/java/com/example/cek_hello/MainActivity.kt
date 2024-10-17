fdbf268e8ab4d10e7942a707118faaffd299800f
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-10-18 06:56:10 +0700

    menambahkan text "Nama"

--- app/src/main/java/com/example/cek_hello/MainActivity.kt
+++ app/src/main/java/com/example/cek_hello/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +20,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cek_hello.ui.theme.Cek_HelloTheme
 
@@ -62,7 +64,12 @@ fun BasicCompose(
             fontSize = 25.sp
         )
         Image(painter = painterResource(id = R.drawable.logo),
-            contentDescription = null )
+            contentDescription = null,
+             modifier = Modifier.size(300.dp)
+        )
+        Text(
+            text = "Nama",
+        )
     }
 
 }
