1320a8a91e53917de7b1b79146ef9892d4fb5f64
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-10-18 07:16:45 +0700

    mengganti warna font menggunakan kode warna hex

--- app/src/main/java/com/example/cek_hello/MainActivity.kt
+++ app/src/main/java/com/example/cek_hello/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -69,7 +70,11 @@ fun BasicCompose(
         )
         Text(
             text = "Nama",
-            fontWeight = FontWeight.Bold,
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF04569F),
+            )
+
         )
     }
 
